633711c57b9b8e53413c2d9bba7663866a562c69
alessandrasil3 <maisasil71@gmail.com>
2024-07-22 11:43:02 -0300

    Update

---
+++ Projeto Festa e Glamour/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Projeto_Festa_e_Glamour.Models;
+using System.Diagnostics;
+
+namespace Projeto_Festa_e_Glamour.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Projeto Festa e Glamour/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projeto_Festa_e_Glamour.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
